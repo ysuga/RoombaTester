99864f9c3d60afb5f1a4647d7fd7cd0d03e48510
Yuki Suga <ysuga@ysuga.net>
2012-12-03 10:00:18 +0900

    First Commit

---
+++ src/RoombaTest.cpp
@@ -0,0 +1,172 @@
+// -*- C++ -*-
+/*!
+ * @file  RoombaTest.cpp
+ * @brief Roomba Test RTC
+ * @date $Date$
+ *
+ * $Id$
+ */
+
+#include "RoombaTest.h"
+
+// Module specification
+// <rtc-template block="module_spec">
+static const char* roombatest_spec[] =
+  {
+    "implementation_id", "RoombaTest",
+    "type_name",         "RoombaTest",
+    "description",       "Roomba Test RTC",
+    "version",           "1.0.0",
+    "vendor",            "sugarsweetrobotics.com",
+    "category",          "experimenta",
+    "activity_type",     "PERIODIC",
+    "kind",              "DataFlowComponent",
+    "max_instance",      "1",
+    "language",          "C++",
+    "lang_type",         "compile",
+    ""
+  };
+// </rtc-template>
+
+/*!
+ * @brief constructor
+ * @param manager Maneger Object
+ */
+RoombaTest::RoombaTest(RTC::Manager* manager)
+    // <rtc-template block="initializer">
+  : RTC::DataFlowComponentBase(manager),
+    m_currentVelIn("currentVel", m_currentVel),
+    m_currentPosIn("currentPos", m_currentPos),
+    m_targetVelOut("targetVel", m_targetVel),
+    m_serviceNameOut("service", m_serviceName)
+
+    // </rtc-template>
+{
+}
+
+/*!
+ * @brief destructor
+ */
+RoombaTest::~RoombaTest()
+{
+}
+
+
+
+RTC::ReturnCode_t RoombaTest::onInitialize()
+{
+  // Registration: InPort/OutPort/Service
+  // <rtc-template block="registration">
+  // Set InPort buffers
+  addInPort("currentVel", m_currentVelIn);
+  addInPort("currentPos", m_currentPosIn);
+  
+  // Set OutPort buffer
+  addOutPort("targetVel", m_targetVelOut);
+  addOutPort("service", m_serviceNameOut);
+  
+  // Set service provider to Ports
+  
+  // Set service consumers to Ports
+  
+  // Set CORBA Service Ports
+  
+  // </rtc-template>
+
+  // <rtc-template block="bind_config">
+  // </rtc-template>
+  
+  return RTC::RTC_OK;
+}
+
+/*
+RTC::ReturnCode_t RoombaTest::onFinalize()
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t RoombaTest::onStartup(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t RoombaTest::onShutdown(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+
+RTC::ReturnCode_t RoombaTest::onActivated(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+
+
+RTC::ReturnCode_t RoombaTest::onDeactivated(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+
+/*
+RTC::ReturnCode_t RoombaTest::onExecute(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t RoombaTest::onAborting(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t RoombaTest::onError(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t RoombaTest::onReset(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t RoombaTest::onStateUpdate(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t RoombaTest::onRateChanged(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+
+
+extern "C"
+{
+ 
+  void RoombaTestInit(RTC::Manager* manager)
+  {
+    coil::Properties profile(roombatest_spec);
+    manager->registerFactory(profile,
+                             RTC::Create<RoombaTest>,
+                             RTC::Delete<RoombaTest>);
+  }
+  
+};
+
+
